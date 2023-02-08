f7f21e84252141bf273c5adf0c9fd9e8e0e83757
canadrian72 <patterson.n.adrian@gmail.com>
2023-02-07 22:47:32 -0500

    Changing to firefox

--- src/utils/hubcontroller.py
+++ src/utils/hubcontroller.py
@@ -3,27 +3,24 @@ from selenium import webdriver
 
 class HubController:
     def __init__(self) -> None:
-        self.options = webdriver.ChromeOptions()
-        self.options.add_experimental_option("useAutomationExtension", False)
-        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        self.options.add_argument("--no-sandbox")
+        self.options = webdriver.FirefoxOptions()
         self.options.add_argument("--kiosk")
         self.options.add_argument("disable-infobars")
         self.options.add_argument("--disable-extensions")
-        self.options.add_argument("--disable-gpu")
         self.options.add_argument("--disable-dev-shm-usage")
-        self.chrome_driver = None
+        self.options.add_argument("--no-sandbox")
+        self.firefox_driver = None
 
     def open_url(self, url: str) -> None:
-        if self.chrome_driver is not None:
-            del self.chrome_driver
+        if self.firefox_driver is not None:
+            del self.firefox_driver
 
-        self.chrome_driver = webdriver.Chrome(options=self.options)
-        self.chrome_driver.get(url)
+        self.firefox_driver = webdriver.Firefox(options=self.options)
+        self.firefox_driver.get(url)
 
     def is_browser_open(self) -> bool:
         try:
-            self.chrome_driver.current_url
+            self.firefox_driver.current_url
             return True
         except:
             return False
