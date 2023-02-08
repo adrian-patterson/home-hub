d4b717e0869131aebd8b2d164d0a30a16d7cbf01
canadrian72 <patterson.n.adrian@gmail.com>
2023-02-08 12:38:42 -0500

    Removing banner

--- src/utils/hubcontroller.py
+++ src/utils/hubcontroller.py
@@ -4,6 +4,10 @@ from selenium import webdriver
 class HubController:
     def __init__(self) -> None:
         self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_experimental_option("useAutomationExtension", False)
+        self.chrome_options.add_experimental_option(
+            "excludeSwitches", ["enable-automation"]
+        )
         self.chrome_options.add_argument("--kiosk")
         self.chrome_options.add_argument("disable-infobars")
         self.chrome_options.add_argument("--disable-extensions")
