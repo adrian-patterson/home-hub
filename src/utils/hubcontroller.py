a442625cce1556dbf4b1483a9138e6f9fd4edc8c
canadrian72 <patterson.n.adrian@gmail.com>
2023-02-07 22:39:12 -0500

    Adding extra options

--- src/utils/hubcontroller.py
+++ src/utils/hubcontroller.py
@@ -3,19 +3,22 @@ from selenium import webdriver
 
 class HubController:
     def __init__(self) -> None:
-        self.chrome_options = webdriver.ChromeOptions()
-        self.chrome_options.add_experimental_option("useAutomationExtension", False)
-        self.chrome_options.add_experimental_option(
-            "excludeSwitches", ["enable-automation"]
-        )
-        self.chrome_options.add_argument("--kiosk")
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("useAutomationExtension", False)
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.options.add_argument("--kiosk")
+        self.options.add_argument("disable-infobars")
+        self.options.add_argument("--disable-extensions")
+        self.options.add_argument("--disable-gpu")
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_argument("--no-sandbox")
         self.chrome_driver = None
 
     def open_url(self, url: str) -> None:
         if self.chrome_driver is not None:
             del self.chrome_driver
 
-        self.chrome_driver = webdriver.Chrome(options=self.chrome_options)
+        self.chrome_driver = webdriver.Chrome(options=self.options)
         self.chrome_driver.get(url)
 
     def is_browser_open(self) -> bool:
